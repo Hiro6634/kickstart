cecbda124954ee7bdc88d146b9508bc023ac8a12
suyamaed <suyama_eduardo@redlink.com.ar>
2022-10-17 23:43:27 -0300

    Basic of Next Routing

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -9,7 +9,7 @@ const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
 const source = fs.readFileSync(campaignPath, 'utf-8');
 const output = solc.compile(source, 1).contracts;
 
-fs.ensureDirSync(buildPath);
+fs.ensureDirSync(   buildPath);
 
 for( let contract in output){
     fs.outputJsonSync(
